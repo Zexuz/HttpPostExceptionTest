0cff6dafb63055ad482149a7a4b83e290b107782
Robin Edbom <robin.edbom@gmail.com>
2018-02-07 16:40:35 +0100

    Init commit.

---
+++ HttpPostExceptionTest/Controllers/DebugController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpPostExceptionTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class DebugController : Controller
+    {
+        
+        
+        [HttpGet("error")]
+        public IActionResult Index()
+        {
+            return StatusCode(500,"Hello, World! From debug controller");
+        }
+    }
+}
---
+++ HttpPostExceptionTest/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpPostExceptionTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            Console.WriteLine("GET Index");
+            throw new Exception();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post()
+        {
+            Console.WriteLine("POST Index");
+            throw new Exception();
+        }
+    }
+}
